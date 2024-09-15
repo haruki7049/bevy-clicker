9af918d04611b71947abbd6b6dfde2d10bb72c49
haruki7049 <tontonkirikiri@gmail.com>
2024-09-15 16:04:35 +0900

    feat(BGM): Add

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ See [rust-toolchain.toml](./rust-toolchain.toml)
 
 ## Validated platforms
 - x86_64-linux
+
+## Assets which is used by this repo
+[静寂の世界へ](https://youtu.be/6is3qL7x9JQ)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use bevy::prelude::*;
+use bevy::audio::PlaybackMode;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(AudioBundle {
+        source: asset_server.load("sounds/bgm.ogg"),
+        settings: PlaybackSettings {
+            mode: PlaybackMode::Loop,
+            ..default()
+        },
+    });
 }
