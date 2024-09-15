11e6f4b2d236a08246e5b3a1e7fa9c750d43e45e
haruki7049 <tontonkirikiri@gmail.com>
2024-09-15 15:37:23 +0900

    chore(cargo-deps): Add bevyengine

--- flake.nix
+++ flake.nix
@@ -11,34 +11,53 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; overlays = [ (import rust-overlay) ]; };
+        lib = pkgs.lib;
         rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
         craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+        dependencies = with pkgs; [
+          pkg-config
+          udev
+          alsa-lib
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          libxkbcommon
+          wayland
+        ];
         src = ./.;
         cargoArtifacts = craneLib.buildDepsOnly {
           inherit src;
+          buildInputs = dependencies;
         };
-        hrtor = craneLib.buildPackage {
+        bevy-clicker = craneLib.buildPackage {
           inherit src cargoArtifacts;
+          buildInputs = dependencies;
           strictDeps = true;
 
           doCheck = true;
         };
         cargo-clippy = craneLib.cargoClippy {
           inherit src cargoArtifacts;
+          buildInputs = dependencies;
           cargoClippyExtraArgs = "--verbose -- --deny warnings";
         };
         cargo-doc = craneLib.cargoDoc {
           inherit src cargoArtifacts;
+          buildInputs = dependencies;
         };
         llvm-cov-text = craneLib.cargoLlvmCov {
           inherit cargoArtifacts src;
+          buildInputs = dependencies;
           cargoExtraArgs = "--locked";
           cargoLlvmCovCommand = "test";
           cargoLlvmCovExtraArgs = "--text --output-dir $out";
         };
         llvm-cov = craneLib.cargoLlvmCov {
           inherit cargoArtifacts src;
+          buildInputs = dependencies;
           cargoExtraArgs = "--locked";
           cargoLlvmCovCommand = "test";
           cargoLlvmCovExtraArgs = "--html --output-dir $out";
@@ -47,28 +66,31 @@
       {
         formatter = treefmtEval.config.build.wrapper;
 
-        packages.default = hrtor;
-        packages.doc = cargo-doc;
-        packages.llvm-cov = llvm-cov;
-        packages.llvm-cov-text = llvm-cov-text;
+        packages = {
+          inherit bevy-clicker llvm-cov llvm-cov-text;
+          default = bevy-clicker;
+          doc = cargo-doc;
+        };
 
         apps.default = flake-utils.lib.mkApp {
           drv = self.packages.${system}.default;
         };
 
         checks = {
-          inherit hrtor cargo-clippy cargo-doc llvm-cov llvm-cov-text;
+          inherit bevy-clicker cargo-clippy cargo-doc llvm-cov llvm-cov-text;
           formatting = treefmtEval.config.build.check self;
         };
 
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShell rec {
           packages = [
             # Rust
             rust
 
             # Nix
             pkgs.nil
-          ];
+          ] ++ dependencies;
+
+          LD_LIBRARY_PATH = lib.makeLibraryPath packages;
 
           shellHook = ''
             export PS1="\n[nix-shell:\w]$ "
